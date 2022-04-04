5bdc9eec32385470ddac6433334d4a0caf47b445
Kevin Pietruszka <kevinpiet13@gmail.com>
2022-04-03 20:17:41 -0400

    Survey page displayed

--- src/Home.js
+++ src/Home.js
@@ -11,7 +11,6 @@ function Home() {
         nav(path);
     }
 
-
     return (
         <>
             <head>
--- src/survey.js
+++ src/survey.js
@@ -1,24 +1,137 @@
 import { useCallback } from "react";
-
 import "survey-react/modern.min.css";
-// import 'survey-react/survey.min.css';
 import { Survey, StylesManager, Model } from "survey-react";
 
 StylesManager.applyTheme("modern");
 
 const surveyJson = {
-  elements: [
-    {
-      name: "FirstName",
-      title: "Enter your first name:",
-      type: "text"
-    },
-    {
-      name: "LastName",
-      title: "Enter your last name:",
-      type: "text"
-    }
-  ]
+    "title": "Threads",
+    "description": "Choose questions if you dare",
+    "logoPosition": "right",
+    "pages": [
+     {
+      "name": "page1",
+      "elements": [
+       {
+        "type": "rating",
+        "name": "question1",
+        "title": "Do you like using using a variety of technology to solve problems?",
+        "rateValues": [
+         1,
+         2,
+         3,
+         4,
+         5
+        ],
+        "minRateDescription": "Not really",
+        "maxRateDescription": "Love that"
+       },
+       {
+        "type": "rating",
+        "name": "question2",
+        "title": "Would you someday want to be able to make network based applications?",
+        "rateValues": [
+         1,
+         2,
+         3,
+         4,
+         5
+        ],
+        "minRateDescription": "Not really",
+        "maxRateDescription": "Love that"
+       },
+       {
+        "type": "rating",
+        "name": "question3",
+        "title": "Does artificial intelligence and computer autonomous systems sound good for the world? ",
+        "rateValues": [
+         1,
+         2,
+         3,
+         4,
+         5
+        ],
+        "minRateDescription": "Not really",
+        "maxRateDescription": "Love that"
+       },
+       {
+        "type": "rating",
+        "name": "question4",
+        "title": "Would you want to use Computer Science in the wide variety of media platforms? ",
+        "rateValues": [
+         1,
+         2,
+         3,
+         4,
+         5
+        ],
+        "minRateDescription": "Not really",
+        "maxRateDescription": "Love that"
+       }
+      ]
+     },
+     {
+      "name": "page2",
+      "elements": [
+       {
+        "type": "rating",
+        "name": "question6",
+        "title": "Do you like to model your situations around you?",
+        "rateValues": [
+         1,
+         2,
+         3,
+         4,
+         5
+        ],
+        "minRateDescription": "Not really",
+        "maxRateDescription": "Love that"
+       },
+       {
+        "type": "rating",
+        "name": "question7",
+        "title": "Do you want to focus on the people who would use your technology?",
+        "rateValues": [
+         1,
+         2,
+         3,
+         4,
+         5
+        ],
+        "minRateDescription": "Not really",
+        "maxRateDescription": "Love that"
+       },
+       {
+        "type": "rating",
+        "name": "question8",
+        "title": "Are you interested in the hardware aspects of computers and learning software to interact with this hardware? ",
+        "rateValues": [
+         1,
+         2,
+         3,
+         4,
+         5
+        ],
+        "minRateDescription": "Not really",
+        "maxRateDescription": "Love that"
+       },
+       {
+        "type": "rating",
+        "name": "question9",
+        "title": "Would you like to develop advanced mathematical skills to understand and develop complex algorithms?",
+        "rateValues": [
+         1,
+         2,
+         3,
+         4,
+         5
+        ],
+        "minRateDescription": "Not really",
+        "maxRateDescription": "Love that"
+       }
+      ]
+     }
+    ]
 };
 
 function ThreadSurvey() {
@@ -31,8 +144,27 @@ function ThreadSurvey() {
   }, []);
 
   survey.onComplete.add(alertResults);
+  return <Survey model={survey} />
+//   return (
+//   <>
+//   <head>
+//       <title>Home</title>
+//   </head>
+//   <body>
+//       <div id="head-bar">
+//           <h7>Georgia Tech</h7>
+//       </div>
 
-  return <Survey model={survey} />;
+//       <div id="page-title">
+//           <h8>Advising Pathways</h8>
+//       </div>
+
+//       <div id="surveyContainer">
+//           <Survey model={survey} />
+//       </div>
+//   </body>
+//   </>
+//   )
 }
 
 export default ThreadSurvey;
