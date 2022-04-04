3215314db9cbfb92b312f0589b6181d9cc1d3297
Kevin Pietruszka <kevinpiet13@gmail.com>
2022-04-03 22:03:08 -0400

    Adds recommendation of threads based off of results

--- src/survey.js
+++ src/survey.js
@@ -1,7 +1,7 @@
 import { useCallback } from "react";
 import "survey-react/modern.min.css";
 import { Survey, StylesManager, Model } from "survey-react";
-
+import { useNavigate } from "react-router-dom";
 StylesManager.applyTheme("modern");
 
 const surveyJson = {
@@ -14,7 +14,7 @@ const surveyJson = {
       "elements": [
        {
         "type": "rating",
-        "name": "question1",
+        "name": "1",
         "title": "Do you like using using a variety of technology to solve problems?",
         "rateValues": [
          1,
@@ -28,7 +28,7 @@ const surveyJson = {
        },
        {
         "type": "rating",
-        "name": "question2",
+        "name": "2",
         "title": "Would you someday want to be able to make network based applications?",
         "rateValues": [
          1,
@@ -42,7 +42,7 @@ const surveyJson = {
        },
        {
         "type": "rating",
-        "name": "question3",
+        "name": "3",
         "title": "Does artificial intelligence and computer autonomous systems sound good for the world? ",
         "rateValues": [
          1,
@@ -56,7 +56,7 @@ const surveyJson = {
        },
        {
         "type": "rating",
-        "name": "question4",
+        "name": "4",
         "title": "Would you want to use Computer Science in the wide variety of media platforms? ",
         "rateValues": [
          1,
@@ -75,7 +75,7 @@ const surveyJson = {
       "elements": [
        {
         "type": "rating",
-        "name": "question6",
+        "name": "5",
         "title": "Do you like to model your situations around you?",
         "rateValues": [
          1,
@@ -89,7 +89,7 @@ const surveyJson = {
        },
        {
         "type": "rating",
-        "name": "question7",
+        "name": "6",
         "title": "Do you want to focus on the people who would use your technology?",
         "rateValues": [
          1,
@@ -103,7 +103,7 @@ const surveyJson = {
        },
        {
         "type": "rating",
-        "name": "question8",
+        "name": "7",
         "title": "Are you interested in the hardware aspects of computers and learning software to interact with this hardware? ",
         "rateValues": [
          1,
@@ -117,7 +117,7 @@ const surveyJson = {
        },
        {
         "type": "rating",
-        "name": "question9",
+        "name": "8",
         "title": "Would you like to develop advanced mathematical skills to understand and develop complex algorithms?",
         "rateValues": [
          1,
@@ -134,37 +134,47 @@ const surveyJson = {
     ]
 };
 
+const threads = ['Devices', 'Information Internetworks', 'Intelligence', 'Media', 'Modeling & Simulation', 'People', 'Systems & Architecture', 'Theory']
+
 function ThreadSurvey() {
+  
   const survey = new Model(surveyJson);
+
   survey.focusFirstQuestionAutomatic = false;
 
+  const nav = useNavigate();
+
+  const backtohome = () =>{ 
+      let path = `/home`; 
+      nav(path);
+  }
+
   const alertResults = useCallback((sender) => {
-    const results = JSON.stringify(sender.data);
+    // const results = JSON.stringify(sender.data);
+
+    let scores = []
+    for (let i = 0; i < 8; i++) {
+      scores.push(sender.data[i+1]);
+    }
+
+    let highest = Math.max(scores);
+    console.log(scores)
+    
+    let results = "We recommend the folowing threads: "
+    for (let i = 0; i < 8; i++) {
+     if (scores[i] == highest) {
+       results = results + threads[i] + " "
+     }
+    }
+
     alert(results);
+
   }, []);
 
   survey.onComplete.add(alertResults);
+  survey.onComplete.add(backtohome)
   return <Survey model={survey} />
-//   return (
-//   <>
-//   <head>
-//       <title>Home</title>
-//   </head>
-//   <body>
-//       <div id="head-bar">
-//           <h7>Georgia Tech</h7>
-//       </div>
-
-//       <div id="page-title">
-//           <h8>Advising Pathways</h8>
-//       </div>
-
-//       <div id="surveyContainer">
-//           <Survey model={survey} />
-//       </div>
-//   </body>
-//   </>
-//   )
+  
 }
 
 export default ThreadSurvey;
